d54bfd40f653c5ed8619ac0308f907d84926ca06
Lin <tzu.hsun.lin046@gmail.com>
2026-01-09 12:59:45 +0800

    style: adjust rwd layout and padding

--- src/pages/landing/Banner.jsx
+++ src/pages/landing/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
       <img
         src={banner}
         alt="banner pic"
-        className="w-full object-cover bg-center max-h-135"
+        className="w-full object-cover bg-center max-h-80 lg:max-h-135"
       />
     </>
   );
--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -19,7 +19,7 @@ const LandingPage = () => {
   return (
     <main className="w-full flex flex-col justify-center items-center">
       <Banner />
-      <section className="max-w-352.5 w-full flex flex-col  py-24 px-12 items-center gap-5 lg:gap-18.75 lg:items-start 2xl:px-0">
+      <section className="max-w-352.5 w-full flex flex-col py-12  px-10 items-center gap-5 lg:py-24 lg:gap-18.75 lg:items-start 2xl:px-0">
         <Title zh={data.name["zh"].split("(")[0]} en={data.name["en"].split("(")[0]} />
         <Intro description={data.description} />
       </section>
