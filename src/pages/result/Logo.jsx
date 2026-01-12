d4652dd0c82a29c8a915528398c11d72e54c6e8c
Lin <tzu.hsun.lin046@gmail.com>
2026-01-12 11:03:23 +0800

    fix: make logo link to homepage

--- src/pages/result/Logo.jsx
+++ src/pages/result/Logo.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Logo = ({ zh, en }) => {
   return (
-    <div className="flex flex-col max-w-40 text-white">
+    <Link className="flex flex-col max-w-40 text-white transition-opacity hover:opacity-70" to="/">
       <p className="text-[16px] font-bold leading-6">{zh}</p>
       <span className="text-[12px] leading-4.5 font-light">{en}</span>
-    </div>
+    </Link>
   );
 };
 export default Logo;
